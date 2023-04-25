f3765dcb0ed4b6307bb58e0b4c81f979e59c1587
Valentina2668 <Valentinka2668@yandex.ru>
2023-04-25 16:05:05 +0300

    Todo-app

--- src/views/Pictures.vue
+++ src/views/Pictures.vue
@@ -1,9 +1,6 @@
 <template>
     <div class="container">
         <div class="cats">
-            <img src="../assets/images/Cat .jpeg">
-        </div>
-        <div class="cats">
             <img src="../assets/images/Cat1.jpg">
         </div>
         <div class="cats">
@@ -11,13 +8,28 @@
         </div>
         <div class="cats">
             <img src="../assets/images/Cat3.jpg">
-        </div> 
+        </div>
         <div class="cats">
             <img src="../assets/images/Cat4.jpg">
         </div> 
         <div class="cats">
             <img src="../assets/images/Cat5.jpg">
         </div> 
+        <div class="cats">
+            <img src="../assets/images/Cat6.jpg">
+        </div> 
+        <div class="cats">
+            <img src="../assets/images/Cat7.jpg">
+        </div>
+        <div class="cats">
+            <img src="../assets/images/Cat8.jpg">
+        </div>
+        <div class="cats">
+            <img src="../assets/images/Cat9.jpg">
+        </div>
+        <div class="cats">
+            <img src="../assets/images/Cat10.jpg">
+        </div>
         
     </div>
 </template>
